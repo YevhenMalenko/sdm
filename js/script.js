1488edcfca87f6daac223c3e58bd133d0af6d084
YevhenMalenko <38867295+YevhenMalenko@users.noreply.github.com>
2020-01-26 22:48:37 +0200

    updated script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
     let burger = document.querySelector('.header__btn'),
-        menu = document.querySelector('.menu');
+        menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu a');
 
     burger.addEventListener('click', function () {
         this.classList.toggle('active');
         menu.classList.toggle('active');
         document.body.classList.toggle('lock');
+        menuItem.forEach(function(item) {
+            item.addEventListener('click', function() {
+                burger.classList.remove('active');
+                menu.classList.remove('active');
+                document.body.classList.remove('lock');
+            });
+        });
     });
 
     //Modal
